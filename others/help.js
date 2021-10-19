b3c4b6f9c4e56afbd8d458d131301432d1b04174
ROMAN <88349886+Myaxi80@users.noreply.github.com>
2021-10-20 01:38:08 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,22 +15,22 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setThumbnail(`https://images-ext-1.discordapp.net/external/4z_YXY0agtILa9_QJQ_yVMVJWn7aVGqXIGleNhwfddM/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/791171444238319618/fa5c61a82be7374394bbc68fad91ed23.png `)
-    .setTitle(`**Help Dj Music**`)
+    .setThumbnail(`https://media.discordapp.net/attachments/814049411008954389/899786175286476830/image0.jpg`)
+    .setTitle(`**Dream bot**`)
     .setDescription(`
 
 **User Commands**
-invite ,support ,about ,ping ,prefix ,uptime.
+invite ,support ,about ,ping ,setprefix ,uptime.
 **Music Commands**
 play,skip,skipto,stop,volume,nowplaying,Eshuffle,search ,resume.
 remove ,queue ,filter ,loop ,lyrics ,radio.
 **Links**
-[Support](https://discord.gg/v9bW8fCc) - [Invite](https://discord.com/api/oauth2/authorize?client_id=791171444238319618&permissions=8&scope=bot)
+[Support](https://discord.gg/dXAyytz2vy) - [Invite](https://discord.com/api/oauth2/authorize?client_id=802927533070352425&permissions=536870387537&scope=bot)
 `)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("YELLOW");
-   message.react("<:setting:830141142687285298>")
+   message.react("✅")
     return message.channel.send(helpEmbed).catch(console.error);
 
   }
