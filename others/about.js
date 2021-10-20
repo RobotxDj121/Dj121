d3f7c9642128d05d07c9588f965c52554d5fa807
ROMAN <88349886+Myaxi80@users.noreply.github.com>
2021-10-20 22:39:57 +0300

    Create about.js

---
+++ others/about.js
@@ -0,0 +1,29 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
+
+  
+
+
+module.exports = {
+  name: "about",
+  aliases: ["abo"],
+  cooldown: 8,
+  description: "**all commands**",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setDescription(`
+
+ [Click here](https://discord.com/api/oauth2/authorize?client_id=802927533070352425&permissions=536870387537&scope=bot) **add bot .**
+`)
+
+  
+   .setColor("YELLOW");
+   message.react("✅")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
